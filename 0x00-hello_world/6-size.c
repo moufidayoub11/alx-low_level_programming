634f4ae7c7c5d5952bc808fd23e92ad96aa71ad7
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-13 10:22:13 +0100

    Task 6. Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints
+ * the size of each data type
+ * using the C standard function printf.
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+
+	return (0);
+}
