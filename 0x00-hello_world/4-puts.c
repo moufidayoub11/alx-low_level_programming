1ee72064462824feae41706ea59e39fb882ec713
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-13 10:07:04 +0100

    Task 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints
+ * "Programming is like building a multilingual puzzle"
+ * using the C standard function puts.
+ *
+ * Return: 0 if successful
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
