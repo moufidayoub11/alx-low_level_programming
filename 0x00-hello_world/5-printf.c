eec59e0f5054e43b57f0892ec57f8c2703996519
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-13 10:17:05 +0100

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -12,9 +12,7 @@
 
 int main(void)
 {
-	char *message = "with proper grammar, but the outcome is a piece of art,";
-
-	printf("%s\n", message);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
