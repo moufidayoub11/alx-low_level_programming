c3f5ac9cf891cf60e793844ff852a372de861297
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-13 10:12:02 +0100

    Task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints
+ * "with proper grammar, but the outcome is a piece of art,"
+ * using the C standard function printf.
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	char messag[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", message);
+
+	return (0);
+}
