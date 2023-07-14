c0ceaf5080f72207817a22b350e752ca49b15c11
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-14 13:18:27 +0100

    Task 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This program prints the the numbers in base 10 (0 - 9),
+ * followed by a new line.
+ *
+ * Return: (0) if successfull
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
