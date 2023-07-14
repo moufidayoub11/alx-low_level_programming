353c23d714ea925fdbc9a725b480d75bb0490286
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-14 13:10:19 +0100

    Task 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This program prints the alphabet in lowercase,
+ * followed by the alphabet in uppercase.
+ * followed by a new line.
+ *
+ * Return: (0) if successfull
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	for (; i <= 122; i++)
+		putchar(i);
+	i = 65;
+
+	for (; i <= 90; i++)
+		putchar(i);
+	
+	putchar('\n');
+
+	return (0);
+}
