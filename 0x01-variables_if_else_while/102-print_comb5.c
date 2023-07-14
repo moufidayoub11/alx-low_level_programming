f93519b1b8953111154efd1c049ac3a0d8f757aa
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-14 14:28:57 +0100

    Task 12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This program prints all possible different
+ * combinations of three digits. 012 and 120 are considered
+ * the same combination of the three digits 0 and 1 and 2
+ * seperated by ', '
+ * followed by a new line.
+ *
+ * Return: (0) if successful
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = 0; j < 99; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+
+				putchar(' ');
+
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
