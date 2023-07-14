f2d6acbdcde4116818280952e8393f5dd28f1b84
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-14 13:26:32 +0100

    Task 8. Hexadecimal;5D

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This program prints the hexadecimal charachters,
+ * followed by a new line.
+ *
+ * Return: (0) if successfull
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57; i++)
+		putchar(i);
+
+	i = 97;
+
+	for (; i <= 102; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
