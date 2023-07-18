56f0552b7b95b608732fb0692061f4eb9a507e1f
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-18 13:11:53 +0100

    Task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - Function that returns the sum of two given integers
+*
+* @n1: The first number
+* @n2: The second number
+*
+* Return: int, the sum of n1 and n2
+*/
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
