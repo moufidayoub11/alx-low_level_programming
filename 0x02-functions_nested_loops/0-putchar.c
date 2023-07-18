cdc448eef0b58563e7ca81e5d62e92731325b390
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-18 10:43:13 +0100

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+* main - Entry point
+*
+* Description: A program that prints '_putchar'
+*
+* Return: 0 (successful)
+*/
+
+int main(void)
+{
+	char string[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(string[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
