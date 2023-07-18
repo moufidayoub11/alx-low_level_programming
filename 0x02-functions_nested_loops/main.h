f2e4287b16359b76ee58861096e288d3ff1c023d
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-18 10:42:32 +0100

    The header file of all functions

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* This is an 'include guard', which prevents problems
+ * if this file is included multiple times
+ */
+
+/* Functions prototype */
+int _putchar(char c);
+
+#endif
