5d5967778226cebb1e29e205402717439d43f8dc
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-20 14:57:29 +0100

    Task 9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: A Fizz Buzz program
+*
+* Return: 0 (successful)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		putchar(' ');
+
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
