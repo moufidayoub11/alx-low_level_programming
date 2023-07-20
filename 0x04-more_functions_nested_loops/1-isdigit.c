e2dd54dc461c77b93fec79a3e0aef9ae4321c778
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-20 11:56:40 +0100

    Task 1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Function that checks if a given char is a digit (0 - 9)
+ *
+ * @c: The char to check
+ *
+ * Return: int, 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1)
+
+	return (0);
+}
