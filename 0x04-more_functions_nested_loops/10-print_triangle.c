b70ab08040d8f510cd8fc57c330286055e1e6760
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-20 15:29:36 +0100

    Task 10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* print_triangle  - Function that draws a diagonal line on the terminal.
+*
+* @n: the length of the line
+*
+* Return: void, this function does't return any value
+*/
+
+void print_triangle(int n)
+{
+	int j, i, k;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		for (i = 0; i < n - i - 1; i++)
+		{
+			_putchar(' ');
+		}
+
+		for (k = 0; k < i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
