4db85dd591ff7b8b0b26248d264eb458b565d468
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-24 16:40:05 +0100

    Task 9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0'; // Copy the null terminator at the end
-	
+	dest[i] = '\0';
+
 	return (dest);
 }
