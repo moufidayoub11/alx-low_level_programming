01b9e45483a0fac92c5cfbea1644a01176b4b7ac
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-24 16:00:04 +0100

    Task every other character of a string, starting with the first character, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _puts2 - Function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * @s: string
+ *
+ * Return: void, No return value
+ */
+
+void _puts2(char *s)
+{
+	int i;
+	int length;
+
+	while (s[length])
+		length++;
+
+	for (i = 0; i < length; i += 2)
+	{
+		if (i >= length || !s[i])
+			break;
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
