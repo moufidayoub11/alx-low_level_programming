af4440f1fee56fd60ef6839fdceb766eddfccaed
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-24 15:08:04 +0100

    Task 3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Function that prints a given string followed by new line
+ *
+ * @s: string
+ *
+ * Return: void, No return value
+ */
+
+void _puts(char *s)
+{
+	while (*s++)
+		_putchar(*(s-1));
+
+	_putchar('\n');
+}
