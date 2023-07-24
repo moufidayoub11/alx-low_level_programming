c1493f9608460dfaaab73e69d57b01ab797e72d7
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-24 16:02:07 +0100

    Task every other character of a string, starting with the first character, followed by a new line.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,7 @@
  * puts2 - Function that prints every other character of a string,
  * starting with the first character, followed by a new line.
  *
- * @s: string
+ * @str: string
  *
  * Return: void, No return value
  */
