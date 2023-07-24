5647ba45642adcfc99cdd92c06c3a12ffe65e5e8
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-24 14:52:02 +0100

    Task 0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that resets the given int pointer
+ *
+ * @*n: pointer to n
+ *
+ * Return: void, No return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98
+}
