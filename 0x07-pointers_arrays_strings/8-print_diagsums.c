f888cea0a7cbd21ef62c385f525964d0ba7698c8
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-31 12:43:30 +0100

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Function that prints the sum of the diagonals af 2d array. 
+ *
+ * @a: the 1d representation of a 2d array of integers.
+ * @size: the size of both rows and columns
+ *
+ * Return: void, returns nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	unsigned int i, diag1_sum = 0, diag2_sum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			diag1_sum = a[i * size + i];
+			diag2_sum = a[i * size + size - 1 - i];
+		}
+		printf("%d, %d\n", diag1_sum, diag2_sum);
+	}
+}
