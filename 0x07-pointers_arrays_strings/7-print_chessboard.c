5e1a6f26574856b177de027155ce1b30efcd7499
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-31 12:25:45 +0100

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Function that prints a chess board
+ *
+ * @a: 2d array representing the chess board.
+ *
+ * Return: void, returns nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
