5ad1c96cf2bc0d051cf6ebe402d5649180d993de
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-07-31 12:46:36 +0100

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,12 +12,12 @@
 
 void print_diagsums(int *a, int size)
 {
-	unsigned int i, diag1_sum = 0, diag2_sum = 0;
+	int i, diag1_sum = 0, diag2_sum = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		diag1_sum = a[i * size + i];
-		diag2_sum = a[i * size + size - 1 - i];
+		diag1_sum += a[i * size + i];
+		diag2_sum += a[i * size + size - 1 - i];
 	}
 	printf("%d, %d\n", diag1_sum, diag2_sum);
 }
