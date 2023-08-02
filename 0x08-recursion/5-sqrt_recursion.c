4f71c0f213710dd635bec578a0b6b845b48dd8ac
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-02 14:46:31 +0100

    Task 5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int _sqrt_helper(int n, int start);
+
+/**
+ * _sqrt_recursion - Function that returns the number x to the power of y.
+ *
+ * @n: the given number.
+ *
+ * Return: int, returns sqrt(n).
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt_helper(n, 0));
+}
+
+/**
+ * _sqrt_helper - Function that helps find the sqrt of n
+ *
+ * @n: the given number.
+ * @start: starting position
+ *
+ * Return: int, returns sqrt(n).
+ */
+
+int _sqrt_helper(int n, int start)
+{
+	long square = start * start;
+
+	if (n == square)
+		return (start);
+	else if (square > n)
+		return (-1);
+	else if (square < n)
+		_sqrt_helper(n, start + 1);
+}
