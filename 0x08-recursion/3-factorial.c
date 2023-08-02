2ff74a5d186a7b78f54b3ea8bc912c89cf3dfb17
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-02 13:22:23 +0100

    Task 3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - Function that returns the factorial of a given number.
+ *
+ * @n: the given number
+ *
+ * Return: int, returns the factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(s - 1));
+}
