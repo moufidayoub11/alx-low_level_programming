0a294bed198052ee7b99d8e97a1c0db106088ca0
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-04 09:03:41 +0100

    Task 0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,14 @@
 /**
  * main - entry point
  *
+ * Description: Program that prints its name, followed by a new line.
+ *
  * @argc: number of arguments
  * @argv: an array of arguments
  *
  * Return: 0 (successful)
  *
-*/
+ */
 
 int main(__attribute__((unused)) int argc, char **argv)
 {
