f57b060ef432955cde09a76b387bde290046e541
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-07 06:02:07 +0100

    Task 1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter.
+ *
+ * @str: the string
+ *
+ * Return: char pointer, pointer to the string or null if failed
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		continue;
+
+	array = malloc(sizeof(char) * i);
+
+	if (!array)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+		array[j] = str[j];
+
+	array[i] = '\0';
+	return (array);
+}
