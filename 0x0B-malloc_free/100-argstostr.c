08f1fb61b88c204dd7687f502061896776b288e5
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-07 07:58:20 +0100

    Task 5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * argstostr - Function that concatenates all the arguments of your program.
+ *
+ * @ac: the size of the args
+ * @av: the args
+ *
+ * Return: char pointer, pointer to the new string or null if failed
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str, *temp;
+	size_t size = 0, i, j;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size_t) ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+
+	str = malloc(sizeof(char) * size + ac);
+
+	if (!str)
+		return (NULL);
+
+	temp = str;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			*temp++ = av[i][j];
+		if (i == ac - 1)
+			*temp++ = '\0';
+		else
+			*temp++ = '\n';
+	}
+
+	return (str);
+}
