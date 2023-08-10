16d03fdbd05b99e717d8c08fce71bcb301c3e13a
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-10 18:00:08 +0100

    Task 0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc
+ * malloc_checked - allocates memory using malloc
+ *
  * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
