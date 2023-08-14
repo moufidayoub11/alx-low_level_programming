d352702384e1c7632fcf8ef2fdcbbc9570a8e711
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-14 15:31:24 +0100

    Task 2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the name of the source file
+ *
+ * return: 0 if successful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
