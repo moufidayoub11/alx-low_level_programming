deec0b86f091c683d8b40d530f6519d1bd0dd236
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-14 16:34:14 +0100

    Task 4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,8 +25,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	newdog->name = strdup(name);
+	if (newdog->name == NULL) {
+		free(newdog);
+		return (NULL);
+	}
+
 	newdog->age = age;
+
 	newdog->owner = strdup(owner);
+	if (newdog->owner == NULL) {
+		free(newdog->name);
+		free(newdog);
+		return (NULL);
+	}
 
 	return (newdog);
 }
