cc05bea420013f225f8f3fc4f43ebffd0a1b55ea
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-16 22:55:32 +0100

    Task 0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that takes a name and the operation function pointer
+ *
+ * @name: name to be printed
+ * @f: operation function pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
