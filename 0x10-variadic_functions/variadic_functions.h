adb021a77f933a454a7f09fdc83651a11bdd7538
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-17 22:19:40 +0100

    Starting files

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VA_H
+#define VA_H
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* VA_H */
