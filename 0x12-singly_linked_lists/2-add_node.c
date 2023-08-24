24bb654cbfc8c5d4bc2a206f3f3aaaa346d4ffc1
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-24 13:36:46 +0100

    Task 2. Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * add_node - Function that adds a new node at the beginning of a list_t list
+ *
+ * @head: the head of a ll
+ * @str: the value of the node
+ *
+ * Return: the number of nodes
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t len = 0;
+
+	while (str[len])
+		len++;
+
+	if (!head && !str)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+	return (*head);
+}
