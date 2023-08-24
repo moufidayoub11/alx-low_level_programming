6c6c103c49adbfc5085955907bb79a965d3b0332
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-24 14:03:37 +0100

    Task 4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_list - Function that frees a linked list
+ *
+ * @head: the head of a ll
+ *
+ * Return: void, returns nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (!head)
+		return;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
