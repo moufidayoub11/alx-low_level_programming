ae15df399779de88c50a8f3aec6f25b10009954a
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-29 10:52:49 +0100

    Tasks 102 to 103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,28 +8,24 @@
  * Return: The address of the node where the loop starts,
  * or NULL if there is no loop
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *fast = head;
-	listint_t *slow = head;
+	listint_t *slow = head, *fast = head;
 
-	if (!head)
-		return (NULL);
-
-	while (fast && fast->next)
+	while (fast != NULL && fast->next != NULL)
 	{
-		fast = fast->next->next;
 		slow = slow->next;
-
-		if (fast == slow)
+		fast = fast->next->next;
+		if (slow == fast)
 		{
 			slow = head;
-			while (slow != head)
+			while (slow != fast)
 			{
 				slow = slow->next;
 				fast = fast->next;
 			}
-			return (fast);
+			return (slow);
 		}
 	}
 
