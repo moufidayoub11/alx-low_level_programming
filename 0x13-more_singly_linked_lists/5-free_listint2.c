73842e3eae24d5dab9bd357d4c36495901509199
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-29 11:09:50 +0100

    All tasks from 1 to 100

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,17 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = (*head);
-		(*head) = (*head)->next;
+		temp = *head;
+		*head = (*head)->next;
 		free(temp);
 	}
 
-	(*head) = NULL;
+	*head = NULL;
 }
