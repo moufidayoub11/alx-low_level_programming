0e612dae4cf274530a0ed1d12dd49dec17845231
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-08-31 10:11:28 +0100

    Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - print dicimal representation of a binary number
+ *
+ * @b: binary number
+ *
+ * Return: the decimal representation
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int len = strlen(b) - 1; /* 0 indexed */
+	int digit = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (len >= 0)
+	{
+		digit = b[len] - '0';
+		if (digit < 0 || digit > 1)
+			return (0);
+
+		result |= (digit << (strlen(b) - 1 - len));
+		len--;
+	}
+
+	return (result);
+}
