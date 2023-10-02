9248ece496045b416a01f6e43ef2bc01339b76bc
moufidayoub11 <136031684+moufidayoub11@users.noreply.github.com>
2023-10-02 15:33:15 +0100

    Task 0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	buff_size = _strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (fd);
 
